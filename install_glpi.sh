9d3049741e3f604dfe902fda010c8298f68f0a75
Loucxf <108999689+Loucxf@users.noreply.github.com>
2023-08-02 23:57:44 +0200

    Update install_glpi.sh

--- install_glpi.sh
+++ install_glpi.sh
@@ -6,8 +6,62 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-# Mise à jour des paquets
+# Installation de MariaDB
 apt update
+apt install -y mariadb-server
 
-# Installation d'Apache
-apt install -y apache2 mariadb-server php
+# Exécution de la configuration sécurisée
+SECURE_MYSQL=$(expect -c "
+set timeout 10
+spawn mysql_secure_installation
+
+expect \"Enter current password for root (enter for none):\"
+send \"\r\"
+
+expect \"Set root password?\"
+send \"Y\r\"
+
+expect \"New password:\"
+send \"YOUR_ROOT_PASSWORD\r\"
+
+expect \"Re-enter new password:\"
+send \"YOUR_ROOT_PASSWORD\r\"
+
+expect \"Remove anonymous users?\"
+send \"Y\r\"
+
+expect \"Disallow root login remotely?\"
+send \"Y\r\"
+
+expect \"Remove test database and access to it?\"
+send \"Y\r\"
+
+expect \"Reload privilege tables now?\"
+send \"Y\r\"
+
+expect eof
+")
+
+echo "$SECURE_MYSQL"
+
+# Vérification de l'état de MariaDB
+if ! systemctl is-active --quiet mariadb; then
+    echo "Une erreur s'est produite lors de la configuration sécurisée de MariaDB."
+    exit 1
+fi
+
+# Demande à l'utilisateur de créer un nouvel utilisateur et une nouvelle base de données
+read -p "Entrez le nom du nouvel utilisateur : " new_user
+read -sp "Entrez le mot de passe du nouvel utilisateur : " new_user_password
+echo ""
+read -p "Entrez le nom de la nouvelle base de données : " new_database
+
+# Création du nouvel utilisateur et de la nouvelle base de données
+mysql -uroot -ppassword<<MYSQL_SCRIPT
+CREATE DATABASE IF NOT EXISTS $new_database;
+CREATE USER '$new_user'@'localhost' IDENTIFIED BY '$new_user_password';
+GRANT ALL PRIVILEGES ON $new_database.* TO '$new_user'@'localhost';
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
+
+echo "Le nouvel utilisateur '$new_user' et la nouvelle base de données '$new_database' ont été créés avec succès."
